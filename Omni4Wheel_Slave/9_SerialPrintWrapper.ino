c4779eaaa13071bbe6c27401603385308159499b
Yohan Prakoso <prakoso.yohan@gmail.com>
2025-08-31 14:52:27 +0800

    Add SerialPrintWrapper

---
+++ Omni4Wheel_Slave/9_SerialPrintWrapper.ino
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+
+void SerialPrint(const char* format, ...) {
+  char buffer[256];
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buffer, sizeof(buffer), format, args);
+  va_end(args);
+  Serial.println(buffer);
+}
